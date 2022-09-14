cbbca3398dd4546dea70f15db317b6e1dad3478f
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2022-09-14 16:58:38 +0700

    hw: JPacman3

--- src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
+++ src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
@@ -19,4 +19,25 @@ public class DirectionTest {
         Direction north = Direction.valueOf("NORTH");
         assertThat(north.getDeltaY()).isEqualTo(-1);
     }
+
+    @Test
+    void testSouth(){
+        Direction south = Direction.valueOf("SOUTH");
+        assertThat(south.getDeltaY()).isEqualTo(1);
+        assertThat(south.getDeltaX()).isEqualTo(0);
+    }
+
+    @Test
+    void testEast(){
+        Direction east = Direction.valueOf("EAST");
+        assertThat(east.getDeltaY()).isEqualTo(0);
+        assertThat(east.getDeltaX()).isEqualTo(1);
+    }
+
+    @Test
+    void testWest(){
+        Direction west = Direction.valueOf("WEST");
+        assertThat(west.getDeltaY()).isEqualTo(0);
+        assertThat(west.getDeltaX()).isEqualTo(-1);
+    }
 }
