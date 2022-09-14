36022cefb9ce4a500e2c5ef353413659272275ce
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2022-09-14 18:37:59 +0700

    hw: JPacman3

---
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -0,0 +1,43 @@
+package nl.tudelft.jpacman.board;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+public class BoardTest {
+
+    private Board board;
+    private final int MAX_WIDTH = 1;
+    private final int MAX_HIGHT = 1;
+
+    @BeforeEach
+    void setup(){
+
+
+    }
+
+    /**
+     * 4.	Create a test class for Board in which you construct a board with
+     * the correct (1×1 is large enough) grid, with a correct BasicSquare on it.
+     * Name your class BoardTest and put it in the src/test folder in the package
+     * nl.tudelft.jpacman.board. Run your test. Since the board is valid your test
+     * should pass. Capture the screenshot of your test result and paste it below.
+     */
+    @Test
+    void testValidBoard(){
+        Square s = new BasicSquare();
+        Square[][] grid = {{s}};
+        board = new Board(grid);
+        assertThat(board.getWidth()).isEqualTo(MAX_WIDTH);
+        assertThat(board.getHeight()).isEqualTo(MAX_HIGHT);
+    }
+
+    @Test
+    void testSquareAt(){
+        Square s = new BasicSquare();
+        Square[][] grid = {{null}};
+        board = new Board(grid);
+        int x = 0, y = 0;
+        Square result = board.squareAt(x,y);
+        assertThat(result).isEqualTo(grid[x][y]);
+    }
+}
