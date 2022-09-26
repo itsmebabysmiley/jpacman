f5bb73e37eb0eb46e5edaf31e9d8037a9b0837ed
itsmebabysmiley <snzzare@gmail.com>
2022-09-26 22:24:11 +0700

    JP4: add one TC

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -40,4 +40,21 @@ public class BoardTest {
         Square result = board.squareAt(x,y);
         assertThat(result).isEqualTo(grid[x][y]);
     }
+
+    @Test
+    void testwithinBorders(){
+        Square s = new BasicSquare();
+        Square[][] grid = {{s}};
+        board = new Board(grid);
+        boolean isValid = board.withinBorders(0,0);
+        assertThat(isValid).isEqualTo(true);
+        isValid = board.withinBorders(0,1);
+        assertThat(isValid).isEqualTo(false);
+        isValid = board.withinBorders(-1,-1);
+        assertThat(isValid).isEqualTo(false);
+        isValid = board.withinBorders(1,0);
+        assertThat(isValid).isEqualTo(false);
+        isValid = board.withinBorders(0,-1);
+        assertThat(isValid).isEqualTo(false);
+    }
 }
