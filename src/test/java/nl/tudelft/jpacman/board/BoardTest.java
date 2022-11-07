12eb31769a4635b1b171891b8cf74eb75ce708e3
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2022-11-07 11:38:33 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -3,6 +3,19 @@ package nl.tudelft.jpacman.board;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+/**
+* <h1>Hello, World!</h1>
+* The HelloWorld program implements an application that
+* simply displays "Hello World!" to the standard output.
+* <p>
+* Giving proper comments in your program makes it more
+* user friendly and it is assumed as a high quality code.
+* 
+*
+* @author  Zara Ali
+* @version 1.0
+* @since   2014-03-31 
+*/
 public class BoardTest {
 
     private Board board;
