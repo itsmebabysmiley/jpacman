dc8aad0274930c80b88881dbc760cf804b4cdda5
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2022-11-15 19:46:49 +0700

    fixed checkStyle

--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -1,17 +1,9 @@
 package nl.tudelft.jpacman.level;
-
-import nl.tudelft.jpacman.PacmanConfigurationException;
 import nl.tudelft.jpacman.board.BoardFactory;
 import nl.tudelft.jpacman.npc.ghost.Blinky;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
-import java.util.ArrayList;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 /**
  * This is a test class for MapParser.
